fa566474145a87fe569319f2324274509db3922a
burak <burak@DESKTOP-JUSEKSP>
2022-08-02 04:47:14 +0300

    CarImages,FileHelper Added

---
+++ WebAPI/Controllers/CarImagesController.cs
@@ -0,0 +1,71 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarImagesController : ControllerBase
+    {
+        private ICarImageService _carImageService;
+        public CarImagesController(ICarImageService carImageService)
+        {
+            _carImageService = carImageService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromForm(Name ="image")] IFormFile file, [FromForm] CarImage carImage)
+        {
+            var result = _carImageService.Add(file, carImage);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update([FromForm(Name = "Image")] IFormFile formFile, [FromForm] CarImage carImage)
+        {
+            var result = _carImageService.Update(formFile, carImage);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete([FromForm]CarImage carImage)
+        {
+            var carDeleteImage = _carImageService.GetByImageId(carImage.Id).Data;
+            var result = _carImageService.Delete(carDeleteImage);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _carImageService.GetAll();
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+
+
+}
